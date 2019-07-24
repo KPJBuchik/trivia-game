c01189f613be05b44754f8845910941f0cb0b848
Kevin Buchik <kbuchik@gmail.com>
2019-07-23 20:03:42 -0700

    added game functions

--- app.js
+++ app.js
@@ -1,35 +1,112 @@
 
-var TriviaQuestions = [
+
+//ids for checking to see if an answer is correct && to to grab a string with a object array filter????
+var triviaQuestions =["1+1","2+2","spicy brown mustard"];
+var triviaAnswers1 = ["2","3","5"]
+var triviaAnswers2 = ["4","5","6"]
+
+
+
+var butt= [
         {question: "1+1",
         answers: ["one","two","three"],
-        correct:1,
+        id:1
         },
         {question:"heres a question",
         answers: ["one answer","another answer"],
-        correct:0,
+        id:2
         }]
- var timer = 0
- var correct=0
- var incorrect=0
+
+
+ var counter= 10;
+ 
+ var correct=0;
+ var incorrect=0;
  var showQuestion;
+ var current=0;
+
+
+
+ $(document).ready(function(){
  
 
-$(document).ready(function(){
+$("#start").click(startGame);
 
-$("#start").click();
 
-setTimeout(FirstQuestion, 1000)
-function FirstQuestion() {
-        $("#box").text("ahoy");
+function timer() {
+
+        if (counter<= 0){
+        
+        nextQuestion()
+        }
+counter--;
 }
+
+
+
+
+function nextQuestion () {
+clearInterval(showQuestion)
+        current++;
+        counter=30;
+        timer()
+        ask();
+}
+
+function ask(){
+if(triviaQuestions[current]){
+$("#questions").text(triviaQuestions[0])
+        
+        setInterval(timer,1000)
+}
+
+timer()
+console.log(counter);
+}
+
+function startGame(){
+
+ask()
+
+ }
+
+
+ 
 })
 
-function StartGame(){
 
 
 
-}
+//populate the dom with questions 
 
+
+
+
+
+
+
+
+
+
+/*
+TriviaQuestions.forEach(function(ask){
+        $("#start").click(ask);
+        var answer =$("<button>");
+        answer.text(ask.answer[1])
+        $("#questions").append(triviaQuestion);
+        var triviaQuestion = $("<p>")
+        answer.text(ask.question[1]);
+        answer.addclass("answer-button")
+        $("#questions").append(answer);
+
+})
+});
+
+
+
+//funciton startGame
+
+//function nextQuestion
 
 
 
