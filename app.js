3bb19ae2971465afc82beb2cfae48055dcb2af7e
Kevin Buchik <kbuchik@gmail.com>
2019-07-24 15:44:50 -0700

    questions and answers loop

--- app.js
+++ app.js
@@ -5,19 +5,20 @@
 
 var triviaQuestions = [
 
-        {
-                question: "1+1",
-                answers: ["one", "two", "three"],
-                id: 1,
+    {
+        question: "1+1",
+        answers: ["one", "two", "three"],
+        id: 1, //dont need id 
 
-        },
+    },
 
-        {
-                question: "heres a question",
-                answers: ["one answer", "another answer"],
-                id: 2,
+    {
+        question: "heres a question",
+        answers: ["one answer", "another answer"],
+        id: 2,
 
-        }]
+    }
+];
 
 
 var counter = 5;
@@ -25,89 +26,78 @@ var counter = 5;
 var correct = 0;
 var incorrect = 0;
 var IntervalId;
-var current = 0;
-var argh = 0;
-
+var currentIndex = 0;
 
 
 $(document).ready(function () {
 
-        
-        $("#start").click(startGame);
-        $("#answer-buttons").click(nextQuestion)
 
-//initializes game 
-        function startGame() {
+    $("#start").click(startGame);
+    $("#answer-buttons").click(nextQuestion)
+
+    //initializes game 
+    function startGame() {
         counter = 10;
         correct = 0;
         incorrect = 0;
         IntervalId;
-        current = 0;
-        argh = 0;
+        currentIndex = 0;
         clearInterval(IntervalId)
         ask()
-                  }
-})
+    }
 
 
-//timer that works 
-function timer() {
+
+    //timer that works 
+    function timer() {
         clearInterval(IntervalId);
-        IntervalId=setInterval(decrement,1000)
-     
-        
+        IntervalId = setInterval(decrement, 1000)
 
-function decrement() {
-        counter--;
-        $('#timer').text(counter)
-        if (counter === 0) {
+
+
+        function decrement() {
+            counter--;
+            $('#timer').text(counter)
+            if (counter === 0) {
                 stop()
+                currentIndex++;
                 ask()
+            }
+
         }
-        
-}
 
-function stop() {
-        clearInterval(IntervalId);
+        function stop() {
+            clearInterval(IntervalId);
 
-}
-}
+        }
+    }
 
 
 
-//i need something to index the questions and answers
-//and loop the questions with a setInteval
-function ask() {
-        
+    //i need a for loop? to index the questions and answers
+    //and loop the questions with a setInteval
+    function ask() {
+
         counter = 5;
-        if (triviaQuestions[current]) {
-                var questionContent = (triviaQuestions[0].question)
-                $("#questions").append(questionContent)
-                       
-
-                var answerContent = Object.values(triviaQuestions[0].answers)
-                
-                $.each(answerContent, function(index, foo){
-                        $('#box').append($('<button class="answer-buttons">'+foo+'</button>'));
-                      })
-                  
+        if (triviaQuestions[currentIndex]) {
+            var questionContent = (triviaQuestions[currentIndex].question)
+            $("#questions").append(questionContent)
 
 
+            var answerContent = Object.values(triviaQuestions[currentIndex].answers)
+
+            $.each(answerContent, function (index, foo) {
+                $('#box').append($('<button class="answer-buttons">' + foo + '</button>'));
+            })
 
         }
         timer()
         console.log(counter);
-        
 
-}
 
-//maybe i dont need this??? 
-function nextQuestion (){
-        current++
-        timer()
-        ask()
+    }
+})
 
-}
 
 
 
