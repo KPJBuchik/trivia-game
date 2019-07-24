0d29daebb1624354e3938e5b691374307165e741
Kevin Buchik <kbuchik@gmail.com>
2019-07-23 21:19:58 -0700

    not much honestly

--- app.js
+++ app.js
@@ -1,78 +1,94 @@
 
 
 //ids for checking to see if an answer is correct && to to grab a string with a object array filter????
-var triviaQuestions =["1+1","2+2","spicy brown mustard"];
-var triviaAnswers1 = ["2","3","5"]
-var triviaAnswers2 = ["4","5","6"]
 
 
 
-var butt= [
-        {question: "1+1",
-        answers: ["one","two","three"],
-        id:1
+
+var triviaQuestions = [
+
+        {
+                question: "1+1",
+                answers: ["one", "two", "three"],
+                id: 1,
+
         },
-        {question:"heres a question",
-        answers: ["one answer","another answer"],
-        id:2
+
+        {
+                question: "heres a question",
+                answers: ["one answer", "another answer"],
+                id: 2,
+
         }]
 
 
- var counter= 10;
- 
- var correct=0;
- var incorrect=0;
- var showQuestion;
- var current=0;
+var counter = 10;
+
+var correct = 0;
+var incorrect = 0;
+var showQuestion;
+var current = 0;
+var argh = 0;
 
 
 
- $(document).ready(function(){
- 
+$(document).ready(function () {
 
-$("#start").click(startGame);
+
+        $("#start").click(startGame);
+        
+
+        function startGame() {
+        counter = 10;
+        correct = 0;
+        incorrect = 0;
+        showQuestion;
+        current = 0;
+        argh = 0;
+        clearInterval(showQuestion)
+        ask()
+                  }
+})
+
 
 
 function timer() {
 
-        if (counter<= 0){
-        
-        nextQuestion()
+        if (counter <= 0) {
+                ask()
         }
-counter--;
+        counter--;
+
 }
 
 
 
 
-function nextQuestion () {
-clearInterval(showQuestion)
-        current++;
-        counter=30;
-        timer()
-        ask();
-}
 
-function ask(){
-if(triviaQuestions[current]){
-$("#questions").text(triviaQuestions[0])
+function ask() {
         
-        setInterval(timer,1000)
+        current++;
+        counter = 30;
+        $('#timer').text(counter)
+        if (triviaQuestions[current]) {
+                var questionContent = Object.values(triviaQuestions[0].question)
+                $("#questions").append(questionContent)
+
+                var answerContent = Object.values(triviaQuestions[0].answers)
+                $("#questions").append(answerContent)
+
+
+
+        }
+
+
+        timer()
+        console.log(counter);
+
+
 }
 
-timer()
-console.log(counter);
-}
 
-function startGame(){
-
-ask()
-
- }
-
-
- 
-})
 
 
 
@@ -118,7 +134,7 @@ TriviaQuestions.forEach(function(ask){
     function FirstQuestion() {
         $("#box").append("<h2>FIRSTQUESTION!</h2>");
         console.log("10 seconds left");
-    
+
 
 
     TriviaQuestions.forEach(function (populate) {
@@ -134,7 +150,7 @@ TriviaQuestions.forEach(function(ask){
 })
 
 
-//reset function 
+//reset function
 
 
 // for loop to generate the questions in ordeR???  method???  TriviaQuestions.forEach(function (populate) {
@@ -143,7 +159,7 @@ TriviaQuestions.forEach(function(ask){
 
 
 
-//logic 
+//logic
 
 ///
 
